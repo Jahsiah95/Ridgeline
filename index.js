8a8743251de004801b20a8df628df9bdd772696a
Jahsiah <jahsiah.c@hotmail.co.uk>
2023-06-11 19:15:08 +0100

    initial commit

--- index.js
+++ index.js
@@ -13,3 +13,36 @@ function menu(){
     }
 }
 
+//hero text reel
+const adjective = document.querySelector('.adjective')
+const hero = document.querySelector('#hero')
+const photoArray = [
+    './media/outside\ extension.jpg',
+    './media/house\ construcion.jpg',
+    './media/extension\ window.webp',
+    './media/housing\ estate.jpg',
+    './media/kitchen-extension-internal.jpg',
+    './media/extension\ scaffold.jpeg'
+]
+const adjArray = ['INTEGRITY','TRANSPARENCY','HONESTY','RESPECT','CARE','EXCELLENCE']
+let i = 0;
+
+hero.style.backgroundImage = `url('${photoArray[photoArray.length-1]}')`
+adjective.textContent = adjArray[adjArray.length-1]
+
+slideshow()
+
+function slideshow(){
+    setTimeout(function(){
+        adjective.textContent = adjArray[i];
+        // hero.style.animation = 'fade 5s infinite'
+        hero.style.backgroundImage = `url('${photoArray[i]}')`
+        i++;
+        if(i < adjArray.length){
+            slideshow();            
+        } else{
+            i = 0
+            slideshow()
+        }
+    }, 5000)
+}
