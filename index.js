2630a6bcd422f94bddbdc1da9625df60abfc5e99
Jahsiah <jahsiah.c@hotmail.co.uk>
2023-06-13 23:23:25 +0100

    initial commit

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ function menu(){
 }
 
 //hero text reel
-const adjective = document.querySelector('.adjective')
-const hero = document.querySelector('#hero')
+const adjective = document.querySelector('.adjective');
+const hero = document.querySelector('#hero');
+const currentImg = document.querySelector('#currentImg');
+const nextImg = document.querySelector('#nextImg');
 const photoArray = [
     './media/outside\ extension.jpg',
     './media/house\ construcion.jpg',
@@ -23,30 +25,47 @@ const photoArray = [
     './media/housing\ estate.jpg',
     './media/kitchen-extension-internal.jpg',
     './media/extension\ scaffold.jpeg'
-]
-
-const adjArray = ['INTEGRITY','TRANSPARENCY','HONESTY','EXPERIENCE','CARE','EXCELLENCE']
+];
+const adjArray = ['INTEGRITY','TRANSPARENCY','HONESTY','EXPERIENCE','CARE','EXCELLENCE'];
 let i = 0;
 
-hero.style.backgroundImage = `url('${photoArray[photoArray.length-1]}')`
+currentImg.style.backgroundImage = `url('${photoArray[photoArray.length-1]}')`
+
+nextImg.style.animation = 'fade 7s ease-out infinite';
+
+
 adjective.textContent = adjArray[adjArray.length-1]
-hero.style.animation = 'fade 5s infinite';
+adjective.style.animation = 'swipe 7s ease-out infinite';
 
 slideshow()
 
 function slideshow(){
-    setTimeout(photos, 5000)
+    setInterval(next, 7000)
+    setTimeout(current, 3500)
 }
 
-function photos(){
+function next(){
     adjective.textContent = adjArray[i];
-    hero.style.animation = 'fade 5s infinite'
-    hero.style.backgroundImage = `url('${photoArray[i]}')`
+
+    nextImg.style.backgroundImage = `url('${photoArray[i]}')`;
+
     i++;
+
     if(i < adjArray.length){
-        slideshow();            
+        // slideshow();            
     } else{
         i = 0;
-        slideshow();
+        // currentImg.style.backgroundImage = `url('${photoArray[0]})`
+        // slideshow();
     }
+}
+
+function current(){
+    setInterval(()=>{
+        if(i>0){
+            currentImg.style.backgroundImage = `url('${photoArray[i-1]}')`;
+        }else{
+            currentImg.style.backgroundImage = `url('${photoArray[photoArray.length-1]}')`
+        }
+    }, 7000)
 }
