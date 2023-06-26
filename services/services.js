7ebe1c6f140398a26f0c6e73cdf134ea74ac1322
Jahsiah <jahsiah.c@hotmail.co.uk>
2023-06-26 17:48:49 +0100

    initial commit

--- services/services.js
+++ services/services.js
@@ -16,22 +16,18 @@ function menu(){
 
 //Enquiries
 function email(){
-    const link = 'mailto: daniel.marshall8697@gmail.com';
+    const link = 'mailto: daniel.marshall@ridgelinestructural.co.uk';
     window.location.href = link
 }
 
 const enquiry = document.querySelectorAll('.enquire');
 enquiry.forEach((enquiry) => enquiry.addEventListener('click',email))
-enquiry.forEach((enquiry) => enquiry.addEventListener('mousedown', (e) => {
-    e.target.style.background = 'darkslategray'
-    e.target.style.color = 'white'
+
+// enquiry.forEach((enquiry) => enquiry.addEventListener('mouseup', (e) => {
+//     e.target.style.background = 'lightgray'
+//     e.target.style.color = 'black'
     
-}))
-enquiry.forEach((enquiry) => enquiry.addEventListener('mouseup', (e) => {
-    e.target.style.background = 'lightgray'
-    e.target.style.color = 'black'
-    
-}))
+// }))
 
 
 
