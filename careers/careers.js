86b761aeb08f657dc2c0b56a419fbfe48165a8b5
Jahsiah <jahsiah.c@hotmail.co.uk>
2023-06-29 23:21:14 +0100

    initial commit

---
+++ careers/careers.js
@@ -0,0 +1,24 @@
+//dropdown
+const dropdown = document.querySelector('.dropdown');
+dropdown.addEventListener('click', menu);
+
+const tabs = document.querySelector('.tabs');
+
+
+function menu(){
+    if(!tabs.getAttribute('id')){
+        tabs.setAttribute('id','display')
+    } else{
+        tabs.removeAttribute('id')
+    }
+}
+
+
+//Enquiries
+function email(){
+    const link = 'mailto: daniel.marshall@ridgelinestructural.co.uk';
+    window.location.href = link
+}
+
+const enquiry = document.querySelectorAll('.enquire');
+enquiry.forEach((enquiry) => enquiry.addEventListener('click', email))
